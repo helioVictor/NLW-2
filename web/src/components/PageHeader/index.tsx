d335cdfd6989baca194f12c50f9018450c36593c
helioVictor <heliohelio1@outlook.com>
2020-08-09 22:12:25 -0300

    Web update

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,6 +8,7 @@ import './styles.css';
 
 interface PageHeaderProps {
   title: String;
+  description?: String;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -22,7 +23,10 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
         </div>
 
         <div className="header-content">
-        <strong>{props.title}</strong>
+          <strong>{props.title}</strong>
+          { props.description && <p>{props.description}</p> }
+
+          { props.children }
         </div>
       </header>
     );
